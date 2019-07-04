8d9380b753c029bc92da94f5e04494b9866b4b18
maurinenami <maurinenami>
2019-07-04 11:56:43 +0300

    added media

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -6,3 +6,5 @@ urlpatterns=[
     url('^$',views.welcome,name='welcome'),
     url(r'^new/community',views.new_community,name='new-community'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
