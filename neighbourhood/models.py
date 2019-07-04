a9e7447da76437d414d94caf72a313a88635672f
maurinenami <maurinenami>
2019-07-04 13:02:43 +0300

    added the profile and posts model

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Community(models.Model):
     image = models.ImageField(upload_to = 'jirani/')
-    name = models.CharField(max_length=55)
+    name_of_neighbourhood = models.CharField(max_length=55)
     location = models.CharField(max_length=50)
     created_by = models.CharField(max_length=50)
     health_dpt = models.CharField(max_length=50)
@@ -10,6 +10,17 @@ class Community(models.Model):
 
     def __str__(self):
         return location
+class Profile(models.Model):
+    profile_photo= models.ImageField(upload_to = 'dp/')
+    location= models.CharField(max_length=50)
+    neighbourhood_name=models.CharField(max_length=50)
+    user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
 
 
+class Post(models.Model):
+    img = models.ImageField(upload_to = 'posts/')
+    profile = models.ForeignKey(User,on_delete=models.CASCADE, null=True)
 
+     def __str__(self):
+        return self.img
+       
