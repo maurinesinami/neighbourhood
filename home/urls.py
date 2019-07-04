a04ecf95ece449f1d3266b66e871dd67f16cf692
maurinenami <maurinenami>
2019-07-04 10:35:35 +0300

    cretaed forms file and created a form for the community model

--- home/urls.py
+++ home/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-
+from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^neighbourhood/',include('neighbourhood.urls')),
+    url(r'',include('neighbourhood.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}), 
 ]
--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Community(models.Model):
+    name = models.CharField(max_length=55)
+    location = models.CharField(max_length=50)
+    created_by = models.CharField(max_length=50)
+    health_dpt = models.CharField(max_length=50)
+    police_dpt = models.CharField(max_length=50)
+
+    def __str__(self):
+        return location
+
+
+
