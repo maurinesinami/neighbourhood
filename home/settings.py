099f1024c454b7dfbdfc58cdbfea818980a8ecba
maurinenami <maurinenami>
2019-07-04 07:08:29 +0300

    configured the app inside settings.py ,created the templates folder and cteated index.html and base.html

--- home/settings.py
+++ home/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'neighbourhood.apps.NeighbourhoodConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
