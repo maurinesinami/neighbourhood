0154f8bcfe758b918790a5835a432343ae2f04a2
maurinenami <maurinenami>
2019-07-05 10:53:38 +0300

    routed to the image file via the image

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -8,6 +8,7 @@ urlpatterns=[
     url('^$',views.welcome,name='welcome'),
     url(r'^new/community',views.new_community,name='new-community'),
     url(r'^new/post$', views.new_post, name='new-post'),
+    url(r'^post$', views.post, name='post'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
