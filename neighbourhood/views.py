fc13152d75a4027b137bbc3a2cfea28834eca744
maurinenami <maurinenami>
2019-07-05 13:20:19 +0300

    created a new profile view

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Community,Post,Profile
 from django.contrib.auth.decorators import login_required
-from . forms import CommunityForm,PostForm
+from . forms import CommunityForm,PostForm,ProfileForm
 from django.contrib.auth.models import User
 
 
@@ -39,3 +39,16 @@ def new_post(request):
 def post(request):
     posts = Post.objects.all()
     return render(request,'post.html',{"posts":posts})
+@login_required(login_url='/accounts/login/')      
+def new_profile(request,id):
+    user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = user
+            profile.save()
+        return redirect('profile')
+    else:
+        form = ProfileForm()
+    return render(request, 'new-profile.html', {"form":form,"user":user})
